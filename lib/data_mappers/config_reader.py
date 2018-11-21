efb97eaced9fa53d69f945ee0e4909dd204bba6b
cleptes <jan.subelj010@gmail.com>
2018-11-21 18:20:28 +0100

    added config validation

--- lib/data_mappers/config_reader.py
+++ lib/data_mappers/config_reader.py
@@ -1,10 +1,89 @@
 import json
-from lib.misc import paths
-from lib.misc import logger
+from lib.misc import paths, logger
+from lib.data_mappers import supported_configs, char_uinput_mapper
 
 log = logger.logger(__name__)
 
 config = None
+
+
+def validate_hotkey_action(do, hotkey_action, keyboard_mapping):
+    if hotkey_action == "nothing" or hotkey_action == "run":
+        return None
+    if hotkey_action == "typeout":
+        error = {}
+        i = 0
+        error["typeout"] = ""
+        for c in do:
+            try:
+                char_uinput_mapper.keys[keyboard_mapping][c]
+            except:
+                error["typeout"] += "Character "+c+" on position "+str(i)+" for keyboard mapping: "+keyboard_mapping+" does not exist! "
+            i+=1
+        if error["typeout"] == "":
+            return None
+        return error
+    if hotkey_action == "shortcut":
+        error = {}
+        press_together = []
+
+        together = do.split(",")
+        for hotkey in together:
+            keys_str = hotkey.split("+")
+            press_together.append(keys_str)
+
+        error["shortcut"] = ""
+        i = 0
+        for combo in press_together:
+            for key_string in combo:
+                i+=1
+                if len(key_string) == 1:
+                    try:
+                        char_uinput_mapper.keys[keyboard_mapping][key_string]
+                    except:
+                        error["shortcut"]+="Character "+key_string+" on position "+str(i)+" for keyboard mapping: "+keyboard_mapping+" does not exist! "
+
+                else:
+                    try:
+                        char_uinput_mapper.keys["control"][key_string]
+                    except:
+                        error["shortcut"]+="Control key "+key_string+" on position "+str(i)+" does not exist! "
+        if error["shortcut"] == "":
+            return None
+        else:
+            return error
+
+
+def validate_config(config_dic : dict):
+    errors = {}
+    keyboard_mapping = config_dic.get("keyboard_mapping", supported_configs.default_hotkey_type)
+    return_config = {"keyboard_mapping": keyboard_mapping}
+    if keyboard_mapping not in supported_configs.keyboard_mappings:
+        return {"keyboard_mapping": keyboard_mapping+" does not exist!"}, None
+
+    for i in range(1,10):
+        setting_for_gkey =config_dic.get("g"+str(i), {})
+        hotkey_type = setting_for_gkey.get("hotkey_type",supported_configs.default_hotkey_type)
+        errors["g" + str(i)] = {}
+        if hotkey_type not in supported_configs.hotkey_types:
+            errors["g" + str(i)]["hotkey_type"] = hotkey_type + " does not exist!"
+        do = setting_for_gkey.get("do",supported_configs.default_hotkey_do)
+        do_validation = validate_hotkey_action(do, hotkey_type, keyboard_mapping)
+        if do_validation:
+            errors["g" + str(i)]["do"] = do_validation.copy()
+            do = ""
+            hotkey_type = "nothing"
+        if len(errors["g" + str(i)]) == 0:
+            errors.pop("g" + str(i))
+        return_config["g" + str(i)] = {"hotkey_type": hotkey_type, "do": do}
+
+    if len(errors) == 0:
+        return None, return_config
+
+    return errors, return_config
+
+
+
 
 def update_config():
     global config
@@ -20,7 +99,10 @@ def read():
     try:
         with open(paths.config_path, "r") as f:
             try:
-                config = json.load(f)
+                err, config = validate_config(json.load(f))
+                log.debug("using config: " + str(config))
+                if err:
+                    log.warning("Error(s) in config: "+str(err))
                 return config
             except:
                 log.error("JSON FILE ERROR, CORRECT JSON! in path: " + paths.config_path)
---
+++ lib/data_mappers/supported_configs.py
@@ -0,0 +1,6 @@
+default_keyboard_mapping = "si"
+keyboard_mappings = ["si", "en"]
+
+default_hotkey_type = "nothing"
+default_hotkey_do = ""
+hotkey_types = ["nothing", "typeout", "shortcut", "run"]
