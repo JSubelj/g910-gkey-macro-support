31204cad3dd8204b41a7cdad38bec395f96046cc
JSubelj <jan.subelj010@gmail.com>
2019-04-11 17:52:01 +0200

    Update installer.sh
    Changed to python3 for no compatibility issues with other distros.

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 
-python setup.py install --record files.txt
+python3 setup.py install --record files.txt
 cp g910-gkeys.service /usr/lib/systemd/system
 systemctl daemon-reload
