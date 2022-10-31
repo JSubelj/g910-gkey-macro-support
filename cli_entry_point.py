3b231fbb760fc6f46eab0d4dd5f1498747a7fbdb
Marcel Grolms <info@suabo.de>
2022-10-31 04:31:23 +0100

    - cleaned cli_entry_point, moved log output to g910_gkey_mapper - removed debug output and no longer needed imports from g910_gkey_mapper, ordered imports, moved uinput init to main - added global definition of program_running in g910_gkey_mapper - moved device.__exit__() to program end to disconnect the device if we run into an error and device was already connected - reworked exit strategy, no direct exit() calls - added JSONDecodeError in config_reader with parse error of config file - changed log level of terminate signals to debug

--- cli_entry_point.py
+++ cli_entry_point.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
-
 import argparse
 import lib.PROJECT_INFO as PROJECT_INFO
 
@@ -23,27 +21,10 @@ def main():
         print("Created by", PROJECT_INFO.AUTHOR)
         print("Version", PROJECT_INFO.VERSION)
     else:
-        from lib.misc import logger
         from lib import g910_gkey_mapper
         from lib.misc import paths
 
-        log = logger.logger("launcher")
-
-        print(f"Starting g910-gkeys, PID: {os.getpid()}, logging at:", paths.logs_path)
-        log.info("------------------------------------------------------------------------------------")
-        log.info(
-            "----------------------STARTED g910-keys-pid:" + str(os.getpid()) + "-----------------------------------")
-        log.info("------------------------------------------------------------------------------------")
-        #pid: ",os.getpid(),
-        #pid_handler.kill_previous()
         g910_gkey_mapper.main()
-        #pid_handler.write_pid(os.getpid())
-        #Daemonize(app="g910-keys", pid=paths.pid_path, action=g910_gkey_mapper.main, keep_fds=[logger.fh.stream.fileno()]).start()
-        '''else:
-            print("g910-keys daemon already exists on pid:",paths.read_pid())
-            print("if you made sure that the program isn't running ('ps -p "+paths.read_pid()+" -o comm=' returns nothing) then manually remove pid file:")
-            print()
-            print("    rm "+paths.pid_path)
-            print()'''
+
 if __name__=="__main__":
     main()
